df3c9175cc8d5589da6d0a0c9c4f003db052f397
harshit2004h <harshitsrivastava2004h@gmail.com>
2025-02-03 21:46:51 +0530

    Refactor Leetcode problem solution and update .gitignore

--- Leetcode/0435_Non_Overlapping_Intervals.cpp
+++ Leetcode/0435_Non_Overlapping_Intervals.cpp
@@ -1,2 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+static bool compare(vector<int> &a, vector<int> &b)
+{
+    return a[1] < b[1];
+}
+
+int eraseOverlapIntervals(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end(), compare);
+
+    int ans = 0;
+    int prev = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i][0] < arr[prev][1])
+            ans++;
+        else
+            prev = i;
+    }
+    return ans;
+}
+
+int main()
+{
+    // Example 1
+    vector<vector<int>> intervals1 = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+    cout << eraseOverlapIntervals(intervals1) << endl; // 1
+
+    // Example 2
+    vector<vector<int>> intervals2 = {{1, 2}, {1, 2}, {1, 2}};
+    cout << eraseOverlapIntervals(intervals2) << endl; // 2
+
+    // Example 3
+    vector<vector<int>> intervals3 = {{1, 2}, {2, 3}};
+    cout << eraseOverlapIntervals(intervals3) << endl; // 0
+
+    // Example 4
+    vector<vector<int>> intervals4 = {{0, 30}, {5, 10}, {15, 20}};
+    cout << eraseOverlapIntervals(intervals4) << endl; // 2
+}
